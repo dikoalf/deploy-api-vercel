46b24b2e013ea1dd173df202a9f6f4374dbfd90f
dikoalf <dikoalfatha@gmail.com>
2024-02-27 14:08:52 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import package
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException, Request, Header
 import pandas as pd
 
 # membuat object FastAPI /  Instance
@@ -7,6 +7,8 @@ app = FastAPI()
 
 # membuat alamat/endpoint
 # contoh get / surveys
+
+API_KEY = 's12345'
 
 # membbuat alamat utama / home endpoint
 
@@ -40,3 +42,18 @@ def createData(data:dict):
     df.loc[len(df.index)] = [data['Name'], data['Location']]
     # df = df.reset_index(drop=True)
     return df.to_dict(orient='records')
+
+@app.get('/see-headers')
+def readHeader(request: Request):
+    headers = request.headers
+
+    return headers
+
+@app.get('/secret')
+def getSecret(api_key: str = Header(None)):
+    if api_key is None or api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+     
+    return {
+        'message':'This is top secret'
+    }
